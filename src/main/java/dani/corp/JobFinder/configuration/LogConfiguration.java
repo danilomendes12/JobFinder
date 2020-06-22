c6e1e4361231c981eebd9a4c647d59fe81c94af9
Danilo Mendes <daperinamendes@gmail.com>
2020-06-22 01:47:54 -0300

    Added request logging

---
+++ src/main/java/dani/corp/JobFinder/configuration/LogConfiguration.java
@@ -0,0 +1,20 @@
+package dani.corp.JobFinder.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LogConfiguration {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
+
+}
